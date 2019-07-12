b184265ae5b052c05f1bd76f9c8427773400753c
1MrSmith <48734090+1MrSmith@users.noreply.github.com>
2019-07-12 10:31:11 +0300

    fix: relocate routes to main container

--- js_machine_front/src/App.tsx
+++ js_machine_front/src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import { Routes } from 'components/Routes';
-import { NavBar } from 'components/NavBar';
+import { NavBar } from 'components/Routes/NavBar';
+import { Partners } from 'components/Partners';
+import { Events } from 'components/Events';
+import { News } from 'components/News';
+import { About } from 'components/About';
+import { Main } from 'components/Main';
+import { Route, Redirect, Switch } from 'react-router';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Routes />
       <NavBar />
+      <Switch>
+        <Route exact={true} path="/" component={Main} />
+        <Route exact={true} path="/about" component={About} />
+        <Route exact={true} path="/news" component={News} />
+        <Route exact={true} path="/events" component={Events} />
+        <Route exact={true} path="/partners" component={Partners} />
+        <Redirect to={'/'} />
+      </Switch>
     </div>
   );
 };
