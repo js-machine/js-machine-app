d485b73982c8484054b9c0f6c12d473281870a24
Ihar <48751046+idzeravianka@users.noreply.github.com>
2019-07-17 11:44:00 +0300

    feat: main component with socials (#7)

--- js_machine_front/src/routes/Main.tsx
+++ js_machine_front/src/routes/Main.tsx
@@ -1,6 +1,9 @@
 import React, { memo } from 'react';
-import Background from '../images/main.jpg';
-import '../styles/main.css';
+import Background from 'images/main.jpg';
+import 'styles/main.css';
+
+import { SocialLinks } from 'components/SocialLinks';
+import { RecentEvents } from 'components/RecentEvents';
 
 const sectionStyle = {
   height: '100vh',
@@ -10,8 +13,40 @@ const sectionStyle = {
   backgroundPosition: 'center',
 };
 
+const events = [
+  {
+    id: '0',
+    date: '01/30/2019',
+    title: 'MITCone 2019',
+    description: 'Одна из самых масштабных конференций Могилева, спикеры из Минска, Гомеля, и других городов',
+  },
+  {
+    id: '1',
+    date: '07/23/2019',
+    title: 'JS JUNGLE',
+    description: 'Отпразнуем День JS разработчика вместе. Вопросы по JS/Front-End и It в целом. Победителям подарки!',
+  },
+  {
+    id: '2',
+    date: '10/15/2019',
+    title: 'Городской пикник',
+    description: 'Встреча могилевских IT-компаний на открытом воздухе в парке Подниколье',
+  },
+  {
+    id: '3',
+    date: '02/04/2019',
+    title: 'The Rolling Scopes Conference',
+    description: 'Интересные доклады, живое общение и конечно же призы!',
+  },
+];
+
 export const Main: React.FC = memo(() => {
   return (
-    <div style={ sectionStyle }/>
+    <div style={sectionStyle}>
+      <SocialLinks />
+      <div className="main__events">
+        <RecentEvents events={events} />
+      </div>
+    </div>
   );
 });
