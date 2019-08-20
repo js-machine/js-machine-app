f38ba13d332b5ef2385d89d9286eb253fb9adc8c
Ihar <48751046+idzeravianka@users.noreply.github.com>
2019-08-20 18:00:03 +0300

    feat: add event page, background to the news page (#22)

--- js_machine_front/src/scenes/news/news.tsx
+++ js_machine_front/src/scenes/news/news.tsx
@@ -1,16 +1,18 @@
 import React, { memo } from 'react';
+import Background from './images/news.jpg';
 
 const sectionStyle = {
-  backgroundColor: 'grey',
+  height: '100vh',
+  backgroundImage: `url(${Background})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
 };
 
 export const News: React.FC = memo(() => {
   return (
     <div style={ sectionStyle }>
-      <h1>News</h1>
-      <div>
-        <h3>Page!</h3>
-      </div>
+      <div className="body"/>
     </div>
   );
 });
