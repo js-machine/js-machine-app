c9bbafe958fd75289de02ed2fbe4e4a903752f48
Nobody <SilDog,S@yandex.ru>
2019-08-25 20:19:12 +0300

    fix: interface name, handle method

--- js_machine_front/src/components/navBar/models/navBar.ts
+++ js_machine_front/src/components/navBar/models/navBar.ts
@@ -1,3 +1,3 @@
 export interface NavBarState {
-    menuIsOpened: boolean;
+    menuOpened: boolean;
 }
--- js_machine_front/src/components/navBar/navBar.tsx
+++ js_machine_front/src/components/navBar/navBar.tsx
@@ -7,14 +7,10 @@ import { FormattedMessage } from 'react-intl';
 import { NavBarState } from './models/navBar';
 
 export class NavBar extends React.PureComponent {
-  public state: NavBarState = {menuIsOpened: false};
+  public state: NavBarState = { menuOpened: false };
 
-  handlingMenu = () => {
-    if (this.state.menuIsOpened) {
-      this.setState({menuIsOpened: false});
-    } else {
-      this.setState({menuIsOpened : true});
-    }
+  handleMenu = () => {
+    this.setState({ menuOpened: !this.state.menuOpened });
   }
   render(): JSX.Element {
     return (
@@ -25,25 +21,25 @@ export class NavBar extends React.PureComponent {
           </NavLink>
         </div>
 
-        <input id="menu__toggle" type="checkbox" checked={this.state.menuIsOpened} onChange={this.handlingMenu}/>
+        <input id="menu__toggle" type="checkbox" checked={this.state.menuOpened} onChange={this.handleMenu} />
         <label className="menu__btn" htmlFor="menu__toggle">
           <span />
         </label>
 
         <div className="menu">
-          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/about" onClick={this.handlingMenu}>
+          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/about" onClick={this.handleMenu}>
             <FormattedMessage id="page.about" />
           </NavLink>
-          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/news" onClick={this.handlingMenu}>
+          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/news" onClick={this.handleMenu}>
             <FormattedMessage id="page.news" />
           </NavLink>
-          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/events" onClick={this.handlingMenu}>
+          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/events" onClick={this.handleMenu}>
             <FormattedMessage id="page.events" />
           </NavLink>
-          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/partners" onClick={this.handlingMenu}>
+          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/partners" onClick={this.handleMenu}>
             <FormattedMessage id="page.partners" />
           </NavLink>
-          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/authorization" onClick={this.handlingMenu}>
+          <NavLink exact={true} className="menu__nav-link" activeClassName="active-link" to="/authorization" onClick={this.handleMenu}>
             <FormattedMessage id="page.signIn" />
           </NavLink>
         </div>
