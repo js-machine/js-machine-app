53a4d5de2f29e5fe1f196b797c18f6f06eb05462
Denis Pasukov <d.pasukov@mail.ru>
2019-08-30 17:56:59 +0300

    feat: update digest news redirecting

--- js_machine_front/src/scenes/news/components/newsPresentation.tsx
+++ js_machine_front/src/scenes/news/components/newsPresentation.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { News } from '../models/news';
+import { Link } from 'react-router-dom';
 
 export const NewsPresentation: React.FC<News> = (props) => {
-    const newsDate: number = new Date(props.date).getDate();
-    const newsMonth: string = new Date(props.date).toLocaleString('ru', { month: 'short' }).toUpperCase();
+  const newsDate: number = new Date(props.date).getDate();
+  const newsMonth: string = new Date(props.date).toLocaleString('ru', {month: 'short'}).toUpperCase();
 
-    return (
-        <div className="news__wrapper">
-            <div className="news__read">
-                <div className="news__read-content">ЧИТАТЬ ></div>
-            </div>
-            <div className="news__content">
-                <div>
-                    <p className="news__day">{newsDate} {newsMonth}</p>
-                </div>
-                <div>
-                    <p className="news__title">{props.title}</p>
-                </div>
-                <div>
-                    <p className="news__description">{props.description}</p>
-                </div>
-            </div>
+  return (
+    <div className="news__wrapper">
+      <Link to="/digestview">
+        <div className="news__read">
+          <div className="news__read-content">ЧИТАТЬ ></div>
         </div>
-    );
+      </Link>
+
+      <div className="news__content">
+        <div>
+          <p className="news__day">{newsDate} {newsMonth}</p>
+        </div>
+        <div>
+          <p className="news__title">{props.title}</p>
+        </div>
+        <div>
+          <p className="news__description">{props.description}</p>
+        </div>
+      </div>
+    </div>
+  )
 };
