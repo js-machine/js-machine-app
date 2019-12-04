55407e125c7ed7884826a6f17da594decf1ee72e
Valentine <valiantsin.haradkou@icloud.com>
2019-12-04 16:55:18 +0300

    feat: firebase analytics (#65)

--- apps/js-machine-front/src/app/firebase.conf.ts
+++ apps/js-machine-front/src/app/firebase.conf.ts
@@ -1,11 +1,7 @@
-// import 'firebase/auth';
-// import firebase from 'firebase/app';
-// import { environment } from '../environments/environment';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/analytics';
+import { environment } from '../environments/environment';
 
-// firebase.initializeApp(environment.firebase);
-
-// if (process.env.REACT_DEV) {
-//   firebase.functions().useFunctionsEmulator('http://localhost:5000');
-// }
-
-// firebase.analytics();
+firebase.initializeApp(environment.firebase);
+firebase.analytics();
--- apps/js-machine-front/src/environments/environment.ts
+++ apps/js-machine-front/src/environments/environment.ts
@@ -3,4 +3,14 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyBW3LKJzZqF4RBkT4_hhvi0TWilMNN8JD0',
+    authDomain: 'js-machine-dev.firebaseapp.com',
+    databaseURL: 'https://js-machine-dev.firebaseio.com',
+    projectId: 'js-machine-dev',
+    storageBucket: 'js-machine-dev.appspot.com',
+    messagingSenderId: '811098528786',
+    appId: '1:811098528786:web:d82a334702178575a3cccb',
+    measurementId: 'G-9RKMVXF2DM',
+  },
 };
