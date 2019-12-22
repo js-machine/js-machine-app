6841e72f097f0f090c137162d57e56a0a7d5c815
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2019-12-22 15:13:01 +0300

    useStyles variable created outside of component

--- apps/js-machine-front/src/app/components/Snowflakes.tsx
+++ apps/js-machine-front/src/app/components/Snowflakes.tsx
@@ -5,64 +5,64 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import { useSnowflakes } from './hooks/useSnowflakes';
 
+const MAIN_YELLOW = '#F2E14C';
+
+const useStyles = makeStyles( theme => ({
+    snow: {
+        position: 'fixed',
+        height: '100%',
+        width: '100%',
+        zIndex: 1,
+        pointerEvents: 'none',
+    },
+    snowSwitcher: {
+        display: 'flex',
+        color: 'white',
+        position: 'absolute',
+        zIndex: 1101,
+        [theme.breakpoints.up('md')]: {
+            top: '29px',
+            left: '38px',
+        },
+        [theme.breakpoints.down('sm')]: {
+            top: '18px',
+            left: '24px',
+        },
+        [theme.breakpoints.down('xs')]: {
+            top: '-2px',
+            left: '-4px',
+            flexDirection: 'row-reverse',
+        },
+    },
+    snowSwitcherLabel: {
+        fontSize: '0.9rem',
+    },
+
+    colorSecondary: {
+        '&$checked': {
+            color: MAIN_YELLOW,
+        },
+    },
+    switchBase: {
+        color: 'white',
+        '&$checked': {
+            color: 'white',
+            '& + $track': {
+                backgroundColor: MAIN_YELLOW,
+                opacity: 1,
+            },
+            "&:hover": {
+                backgroundColor: '#f2e14c4d',
+            },
+        },
+    },
+    track: {},
+    checked: {},
+}));
+
 export function Snowflakes() {
 
     const {isSnowing, handleSnowSwitch} = useSnowflakes();
-
-    const MAIN_YELLOW = '#F2E14C';
-
-    const useStyles = makeStyles( theme => ({
-        snow: {
-            position: 'fixed',
-            height: '100%',
-            width: '100%',
-            zIndex: 1,
-            pointerEvents: 'none',
-        },
-        snowSwitcher: {
-            display: 'flex',
-            color: 'white',
-            position: 'absolute',
-            zIndex: 1101,
-            [theme.breakpoints.up('md')]: {
-                top: '29px',
-                left: '38px',
-            },
-            [theme.breakpoints.down('sm')]: {
-                top: '18px',
-                left: '24px',
-            },
-            [theme.breakpoints.down('xs')]: {
-                top: '-2px',
-                left: '-4px',
-                flexDirection: 'row-reverse',
-            },
-        },
-        snowSwitcherLabel: {
-            fontSize: '0.9rem',
-        },
-
-        colorSecondary: {
-            '&$checked': {
-                color: MAIN_YELLOW,
-            },
-        },
-        switchBase: {
-            color: 'white',
-            '&$checked': {
-                color: 'white',
-                '& + $track': {
-                    backgroundColor: MAIN_YELLOW,
-                    opacity: 1,
-                },
-                "&:hover": {
-                    backgroundColor: '#f2e14c4d',
-                },
-            },
-        },
-        track: {},
-        checked: {},
-    }));
 
     const classes = useStyles();
 
