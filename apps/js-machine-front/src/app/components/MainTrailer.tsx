f7e70814ebdf912e1ad3eb821bc53d91edc35641
Denis Pasukov <d.pasukov@mail.ru>
2020-01-14 23:14:20 +0300

    fix: video aligning

--- apps/js-machine-front/src/app/components/MainTrailer.tsx
+++ apps/js-machine-front/src/app/components/MainTrailer.tsx
@@ -4,7 +4,9 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles({
   root: {
     position: 'fixed',
-    top: 0,
+    top: '50%',
+    right: '50%',
+    transform: 'translate(50%,-50%)'
   },
 });
 
