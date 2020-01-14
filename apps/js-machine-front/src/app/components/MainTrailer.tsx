491e354388c20346baf1af3c781e80a0a9fc01ed
Dzianis Pasiukou <d.pasukov@mail.ru>
2020-01-14 10:34:11 +0300

    feat: refactor main scenes to store, add video on main page (#78)

---
+++ apps/js-machine-front/src/app/components/MainTrailer.tsx
@@ -0,0 +1,19 @@
+import React, { memo } from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    position: 'fixed',
+    top: 0,
+  },
+});
+
+export const MainTrailer = memo(() => {
+  const classes = useStyles();
+
+  return (
+    <video className={classes.root} autoPlay muted loop id="myVideo">
+      <source src="assets/main.mp4" type="video/mp4" />
+    </video>
+  );
+});
--- apps/js-machine-front/src/app/stores/ui.store.ts
+++ apps/js-machine-front/src/app/stores/ui.store.ts
@@ -1,1 +1,13 @@
-export class UiStore {}
+import { action, observable } from "mobx";
+import { RootStore } from "@js-machine-app/front/stores/root.store";
+
+export class UiStore {
+  @observable public isPageLoading?: boolean;
+
+  public constructor(private rootStore: RootStore) {
+  }
+
+  @action public setIsLoading = (isPageLoading: boolean) => {
+    this.isPageLoading = isPageLoading;
+  }
+}
