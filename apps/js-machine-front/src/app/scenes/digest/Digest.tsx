579dbdfc644861941c2d240ebf7ad1c754db1160
Denis Pasukov <d.pasukov@mail.ru>
2020-01-14 23:09:21 +0300

    feat: add meta tags for digest

--- apps/js-machine-front/src/app/scenes/digest/Digest.tsx
+++ apps/js-machine-front/src/app/scenes/digest/Digest.tsx
@@ -11,6 +11,7 @@ import { Options } from './Options';
 import { Theme } from '@material-ui/core/styles';
 import { getDigestsById } from '@js-machine-app/data-service';
 import { Loader } from '../../components/Loader';
+import MetaTags from 'react-meta-tags';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -66,7 +67,7 @@ interface Props {
   match: { params: { id: string } };
 }
 
-export const Digest = memo(({ history, match }: Props) => {
+export const Digest = memo(({history, match}: Props) => {
   const classes = useStyles();
   const [markdown, setMarkdown] = useState();
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -87,20 +88,27 @@ export const Digest = memo(({ history, match }: Props) => {
   );
 
   return (
-    <Grid className={classes.root} container>
-      <Hidden smDown>
-        <Grid className={classes.leftSide} item md={3}>
-          <BackButton className={classes.backButton} onClick={ handleBackButton }>
-            <FormattedMessage id="digest.back" />
-          </BackButton>
-          <Options className={classes.options} />
+    <div>
+      <MetaTags>
+        <meta property="og:title" content="JS Machine Digest" />
+        <meta property="og:image"
+              content="https://firebasestorage.googleapis.com/v0/b/js-machine-app.appspot.com/o/%20Digest%20Cycle%20News.png?alt=media&token=bd7c00dc-c389-4227-891b-dcb584ab4946" />
+      </MetaTags>
+      <Grid className={classes.root} container>
+        <Hidden smDown>
+          <Grid className={classes.leftSide} item md={3}>
+            <BackButton className={classes.backButton} onClick={handleBackButton}>
+              <FormattedMessage id="digest.back" />
+            </BackButton>
+            <Options className={classes.options} />
+          </Grid>
+        </Hidden>
+        <Grid className={classes.rightSide} item md={9}>
+          <DateAndView views={25} />
+          <Loader isLoading={isLoading} />
+          <Markdown markdown={markdown} />
         </Grid>
-      </Hidden>
-      <Grid className={classes.rightSide} item md={9}>
-        <DateAndView views={25} />
-        <Loader isLoading={isLoading} />
-        <Markdown markdown={markdown} />
       </Grid>
-    </Grid>
+    </div>
   );
 });
