c516763839ecdad76e32bb6fc1010f5e085b72df
Denis Pasukov <d.pasukov@mail.ru>
2020-01-28 22:14:02 +0300

    feat: add years and background for video

--- apps/js-machine-front/src/app/components/NavDesktop.tsx
+++ apps/js-machine-front/src/app/components/NavDesktop.tsx
@@ -1,30 +1,47 @@
-import React, { memo } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import { makeStyles } from '@material-ui/core/styles';
+import { useStores } from '@js-machine-app/front/stores'
+import { observer } from "mobx-react-lite";
 
 const useStyles = makeStyles(theme => ({
-  link: {
-    textDecoration: 'none',
-    color: theme.palette.primary.contrastText,
-    fontWeight: 'bold',
-    marginLeft: 60,
-  },
-  activeNavLink: {
-    // color should be in theme
-    color: '#f2e14c',
-  },
-}));
+    navDesktop: {
+      padding: '10px 20px'
+    },
+    navDesktopWithBackground: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      padding: '10px 20px'
+    },
+    link: {
+      textDecoration: 'none',
+      color: theme.palette.primary.contrastText,
+      fontWeight: 'bold',
+      marginLeft: 60,
+    },
+    firstLink: {
+      textDecoration: 'none',
+      color: theme.palette.primary.contrastText,
+      fontWeight: 'bold',
+      marginLeft: 0,
+    },
+    activeNavLink: {
+      // color should be in theme
+      color: '#f2e14c',
+    },
+  }))
+;
 
-export const NavDesktop = memo(function NavDesktop() {
+export const NavDesktop = observer(function NavDesktop() {
   const classes = useStyles();
+  const {routerStore} = useStores();
 
   return (
-    <>
+    <div className={routerStore.location.pathname === '/' ? classes.navDesktopWithBackground : classes.navDesktop}>
       <NavLink
         exact
         to="/about"
-        className={classes.link}
+        className={classes.firstLink}
         activeClassName={classes.activeNavLink}
       >
         <FormattedMessage id="page.about" />
@@ -45,6 +62,6 @@ export const NavDesktop = memo(function NavDesktop() {
       >
         <FormattedMessage id="page.events" />
       </NavLink>
-    </>
+    </div>
   );
 });
