bb1b0519bf54d9b0baad511b69a5f7df659ec04e
Denis Pasukov <d.pasukov@mail.ru>
2020-01-30 18:01:07 +0300

    fix: comments

--- apps/js-machine-front/src/app/components/NavDesktop.tsx
+++ apps/js-machine-front/src/app/components/NavDesktop.tsx
@@ -4,23 +4,24 @@ import { FormattedMessage } from 'react-intl';
 import { makeStyles } from '@material-ui/core/styles';
 import { useStores } from '@js-machine-app/front/stores';
 import { observer } from "mobx-react-lite";
+import clsx from 'clsx';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(({spacing, palette}) => ({
     navDesktop: {
-      padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
+      padding: `${spacing(1)}px ${spacing(2)}px`,
     },
     withBackground: {
       backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
     link: {
       textDecoration: 'none',
-      color: theme.palette.primary.contrastText,
+      color: palette.primary.contrastText,
       fontWeight: 'bold',
-      marginLeft: theme.spacing(7),
+      marginLeft: spacing(7),
     },
     firstLink: {
       textDecoration: 'none',
-      color: theme.palette.primary.contrastText,
+      color: palette.primary.contrastText,
       fontWeight: 'bold',
       marginLeft: 0,
     },
@@ -37,7 +38,9 @@ export const NavDesktop = observer(function NavDesktop() {
 
   return (
     <div
-      className={`${classes.navDesktop} ${routerStore.location.pathname === '/' ? classes.withBackground : ''}`}>
+      className={clsx(classes.navDesktop, {
+        [classes.withBackground]: routerStore.location.pathname === '/',
+      })}>
       <NavLink
         exact
         to="/about"
