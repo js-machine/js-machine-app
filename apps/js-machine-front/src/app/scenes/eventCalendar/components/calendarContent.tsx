20ab6c865874ab1b5aadad551264b44ac5daf46a
curly-man <anton_smille@mail.ru>
2020-07-09 16:04:10 +0300

    Event calendar styles (#90)
    * feat: a new route calendar
    
    * feat: add new route: calendar
    
    * fix: type errors
    
    * fix: remove comments
    
    * fix: set culture to 'ru-RU'
    
    * feat: made adaptive width and height

--- apps/js-machine-front/src/app/scenes/eventCalendar/components/calendarContent.tsx
+++ apps/js-machine-front/src/app/scenes/eventCalendar/components/calendarContent.tsx
@@ -3,24 +3,14 @@ import { Event } from '@js-machine-app/models';
 import { PropsEventContent } from '../models/eventContent';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../styles/calendar.css'
 import moment from 'moment'
-
-const calendar = {
-  'display': "inline-block",
-  'margin': "128px 120px",
-  'width': '600px',
-  'padding': "10px",
-  'backgroundColor': '#fff',
-  'height': '450px',
-};
-
 
 export const CalendarContent: React.FC<PropsEventContent> = (
   props: PropsEventContent,
 ) => {
   const localizer = momentLocalizer(moment)
   const events = props.events.map((event: Event) => {
-    console.log(event)
     const startDate = new Date(event.date);
     const endDate = new Date(event.date);
     endDate.setHours(endDate.getHours() + 2);
@@ -33,16 +23,20 @@ export const CalendarContent: React.FC<PropsEventContent> = (
     }
   });
   return (
-    <div style={calendar}>
-        <Calendar
-            rtl={false}
-            events={events}
-            culture={'ru-RU'}
-            defaultDate={new Date()}
-            localizer={localizer}
-            startAccessor="start"
-            endAccessor="end"
-        />
+    <div className='body'>
+      <div className="calendarContainer">
+        <div className="calendar">
+          <Calendar
+              rtl={false}
+              events={events}
+              culture={'ru-RU'}
+              defaultDate={new Date()}
+              localizer={localizer}
+              startAccessor="start"
+              endAccessor="end"
+          />
+        </div>
+      </div>
     </div>
   );
 }
